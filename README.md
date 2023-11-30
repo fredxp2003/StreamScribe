801e4c0358908c5f69ac29cf7a070d46b204fd78
Logan <translucenttomato@gmail.com>
2023-11-30 18:02:38 -0500

    The GUI update
    Added GUI code

--- README.md
+++ README.md
@@ -2,14 +2,17 @@
 
 StreamScribe is a simple, lightweight, and easy to use application that allows you to record audio streams from the internet.  Currently, it is a CLI application, but a GUI is planned for the future.
 
-## Installation
-Be sure that Python 3.6+ is installed on your system.  Then, install the dependencies with `pip install -r requirements.txt`.  Finally, run `python3 streamscribe.py` to start the application.
+## Pre-requisites
+StreamScribe requires the following to be installed on your system:
+- [Python 3.6+](https://www.python.org/downloads/) (If running from source)
+ - If running from source, you will also need to run `pip install -r requirements.txt` in the project directory to install the required Python packages.
+- [VLC](https://www.videolan.org/vlc/index.html)
 
-## Setup
-StreamScribe uses a configuration file to store the stream URL.  The default url points to WXOU, a radio station at Oakland University.  To change the URL, edit the select the second option in the main menu when running the application.  Then enter the URL of the stream you want to record.
+## Installation
+To install StreamScribe, download the latest release for your system from the [releases page](https://github.com/fredxp2003/StreamScribe/releases).  
 
 ## Usage
-To record a stream, select the first option in the main menu.  The stream will be recorded to a file in the same directory as the application.  The file will be named `[chosen name].mp3` by default.
+After opening the program, all you need to do is enter the URL of the stream you want to record.  Then, after hitting record, you will be prompted to enter a name for the file.  The file will be saved in the output directory of the application, which can be opened from StreamScribe.
 
 ## License
 StreamScribe is licensed under the MIT License.  See the LICENSE file for more information.
--- main.py
+++ main.py
@@ -1,72 +1,131 @@
 import requests
 import threading
-import configparser as config
 import vlc
+from tkinter import *
+from tkinter import simpledialog
+import datetime
+import time
+import os
+import subprocess
 
-print("Welome to StreamScribe! (version 0.0.2)")
+print("Welome to StreamScribe! (version 1.0.0-beta2): The GUI Update")
 
-# Read config file
-config = config.ConfigParser()
-config.read("config.ini")
-url = config["settings"]["url"]
+try:
+    with open(".url", "r") as f:
+        url = f.read()
+except FileNotFoundError:
+    with open(".url", "a") as f: 
+        pass  
+
+    url = ""
+
+recording_flag = True
+filename_input = None
+# See if the output folder exists, if not, create it
+path = os.path.join(os.getcwd(), "output")
+if not os.path.exists(path):
+    os.mkdir(path)
 
 
-# Initialize VLC
-instance = vlc.Instance()
-player=instance.media_player_new()
-media=instance.media_new(url)
-player.set_media(media)
+def vlc_init(url):
+    instance = vlc.Instance()
+    player=instance.media_player_new()
+    media=instance.media_new(url)
+    player.set_media(media)
+    return player
 
-def record(stream, filename):
-    threading.Thread(target=player.play).start()
-    with open(f"{filename}.mp3", "wb") as f:
-        for chunk in stream.iter_content(chunk_size=1024):
+def start_recording():
+    global recording_flag, filename_input
+    recording_flag = True
+    filename_input = simpledialog.askstring("Filename", "Enter a filename for the recording:")
+
+    #thread instance
+    new_recording_thread = threading.Thread(target=record)
+    new_recording_thread.start()
+
+def timer():
+    global recording_flag
+    seconds = 0
+    while recording_flag:
+        time.sleep(1)
+        seconds += 1
+        minutes = seconds // 60
+        seconds_part = seconds % 60
+        elapsed_time = f"{minutes:02d}:{seconds_part:02d}"
+        label_text.set(f"Recording...  {elapsed_time}")
+    label_text.set(f"File saved as {filename}.mp3")
+
+def record():
+    global recording_flag, filename_input, filename
+    filename = filename_input if filename_input else datetime.datetime.now().strftime("%Y%m%d_%H%M%S")
+    url = url_box.get()
+    with open(".url", "w") as f:
+        f.write(url)
+
+    # Initialize VLC
+    player = vlc_init(url)
+
+    # init stream 
+    stream = requests.get(url, stream=True)
+    player.play()
+
+    label_text.set(f"Recording...  00:00")
+    threading.Thread(target=timer).start()
+    OUTPUT_PATH = os.path.join("output", f"{filename}.mp3")
+    with open(OUTPUT_PATH, "wb") as f:
+        for chunk in stream.iter_content(chunk_size=4096):
+            if not recording_flag:
+                player.stop()
+                break  
             if chunk:
                 f.write(chunk)
+    print("Done recording!")
+
+def stop():
+    global recording_flag
+    print("Stopping...")
+    recording_flag = False
+
+def open_output_folder():
+    # WINDOWS
+    if os.name == 'nt':
+        os.startfile(path)
+    # MACOS / LINUX
+    elif os.name == 'posix':
+        subprocess.call(['open', path])
 
 
-def settings():
-    print("Settings")
-    choice = input("Would you like to change the URL? (y/n): ")
-    if choice.lower() == "y":
-        url = input("Enter a new URL: ")
-        config["settings"]["url"] = url
-        with open("config.ini", "w") as configfile:
-            config.write(configfile)
-        print("Settings saved!")
-        return
-    else:
-        print("No changes were made.")
-        return
+# The Tkinter stuff :)
+root = Tk()
+root.title("StreamScribe")
+root.iconbitmap("icon.ico")
+root.geometry("500x250")
 
+root.configure(bg='#333645') 
 
-# Main loop
-while True:
-    print(
-        """
-    1. Record
-    2. Settings
-    3. Exit
-    """
-    )
-    choice = input("Enter a choice: ")
-    if choice == "1":
-        filename = input("Enter name for recording: ")
-        stream = requests.get(url, stream=True)
-        thread = threading.Thread(target=record, args=(stream, filename))
-        thread.start()
+main_frame = Frame(root, bg='#333645')
+main_frame.pack(padx=20, pady=20)
 
-        print(f"Recording started on {url}.")
-        input("Press enter to stop recording...")
-        stream.close()
-        player.stop()
-        print(f"Recording stopped.  File saved as {filename}.mp3")
+label_text = StringVar()
+label_text.set("Welcome to StreamScribe!")
 
-    elif choice == "2":
-        settings()
+title_font = ("Arial", 18, "bold")
+da_label = Label(main_frame, textvariable=label_text, bg='#333645', fg='#FFFFFF', font=title_font)
+da_label.grid(row=0, column=0, columnspan=2, pady=10) 
 
-    elif choice == "3":
-        exit()
-    else:
-        print("Invalid choice. Please try again.")
-        continue
+button_font = ("Arial", 16)
+record_button = Button(main_frame, text="Record", command=start_recording, font=button_font, bg='#007BFF', fg='#FFFFFF', relief=FLAT)
+record_button.grid(row=1, column=0, padx=10, pady=10)  
+
+url_font = ("Arial", 14)
+url_box = Entry(main_frame, width=40, font=url_font)
+url_box.grid(row=2, column=0, columnspan=2, pady=10)
+url_box.insert(0, url)
+
+stop_button = Button(main_frame, text="Stop", command=stop, font=button_font, bg='#FF6347', fg='#FFFFFF', relief=FLAT)
+stop_button.grid(row=1, column=1, padx=10, pady=10)
+
+open_folder_button = Button(main_frame, text="Open Folder", command=open_output_folder, font=button_font, bg='#28B463', fg='#FFFFFF', relief=FLAT)
+open_folder_button.grid(row=3, column=0, columnspan=2, padx=10, pady=10)
+
+root.mainloop()
